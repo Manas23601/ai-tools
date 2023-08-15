f72d42e9e47d7ac6581585d1eccaf0eb7240c3cb
Manas Sivakumar <manas23601@gmail.com>
2023-08-15 23:08:22 +0530

    Update Chunker.py

--- ChunkingExamples/Chunker.py
+++ ChunkingExamples/Chunker.py
@@ -5,11 +5,15 @@ import sys, pathlib,fitz
 with fitz.open("./NCF2023.pdf") as doc:  # open document
     text = chr(12).join([page.get_text() for page in doc])
 # write as a binary file to support non-ASCII characters
-pathlib.Path("output" + ".txt").write_bytes(text.encode())
+pathlib.Path("PyMuPDF_output" + ".txt").write_bytes(text.encode())
 
 '''
 Python PDFQuery Implementation
 '''
+import pdfquery
+pdf = pdfquery.PDFQuery('NCF2023.pdf')
+pdf.load()
+pdf.tree.write('pdfquery_output.xml', pretty_print = True)
 
 '''
 PDF PdfPlumber Implementation
